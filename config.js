c6fea61796c9c12c5fc7e3c68670ef6ada5e21b8
joffrealban <joffre.alban@hotmail.com>
2024-11-11 22:43:37 -0500

    Correccion menu desplegable para dispositivos mobiles

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // esta parte, permite que se ejecute el codigo cuando se halla cargado la pagina
-$(function() {
-    // esta parte es la que controla cuando se mueve el scroll y ejecuta una función
-    $(document).scroll(function() {
-      // aca se pregunta si el scroll se movio de pa bajo.
-      if ($(this).scrollTop() > 1) {
-        // esta parte cambia el atributo "src" de la etiqueta "img" 
-        $('#logo').attr('src', './images/logo_1.png')
-      }
-      if ($(this).scrollTop() < 1) {
-        $('#logo').attr('src', './images/logo_blanco1.png');
-      }
-    });
+$(function () {
+  // esta parte es la que controla cuando se mueve el scroll y ejecuta una función
+  $(document).scroll(function () {
+    // aca se pregunta si el scroll se movio de pa bajo.
+    if ($(this).scrollTop() > 1) {
+      // esta parte cambia el atributo "src" de la etiqueta "img" 
+      $('#logo').attr('src', './images/logo_1.png')
+    }
+    if ($(this).scrollTop() < 1) {
+      $('#logo').attr('src', './images/logo_blanco1.png');
+    }
   });
+});
